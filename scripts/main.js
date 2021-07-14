350c5f582dd710732fa28114499c18ed90c40ef4
skyneton <67633420+skyneton@users.noreply.github.com>
2021-07-14 13:58:37 +0900

    UPLOAD

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,22 @@
 const getPageImage = (reader, page) => {
-    const img = document.createElement("img");
-    document.body.append(img);
+    const canvas = document.createElement("img");
+    document.body.appendChild(canvas);
+
     reader.getPageImage({
         success(url) {
-            img.src = url;
-            if(reader.numImageConverts >= reader.numPages) console.log(Date.now() - reader.pdfLoadedTime, "ms");
+            canvas.src = url;
+            if(reader.numImageConverts >= reader.numPages) reader.close();
         }, error(e) {
-            img.remove();
+            canvas.remove();
             console.error(e);
-            if(reader.numImageConverts >= reader.numPages) console.log(Date.now() - reader.pdfLoadedTime, "ms");
-        }
-    }, page);
+            if(reader.numImageConverts >= reader.numPages) reader.close();
+        },
+        scale: 96.0 / 72.0 * 0.8,
+        maxWidth: 800,
+        maxHeight: 800,
+        toURL: true,
+        page,
+    });
 };
 
 const showPDFImages = (reader, pages) => {
@@ -19,15 +25,40 @@ const showPDFImages = (reader, pages) => {
     }
 };
 
-document.getElementById("inputFile").onchange = event => {
-    const reader = new PDFReader({
-        numWorkers: 3,
-        success(pages) {
-            showPDFImages(reader, pages);
-        },error(e) {
-            console.error(e);
-            reader.close();
-        }
-    }, event.target.files[0] || event.dataTransfer.files[0]);
+const getDocument = file => {
+    return new Promise((resolve, reject) => {
+        const reader = new PDFReader({
+            numWorkers: 3,
+            success() {
+                resolve(reader);
+            },error(e) {
+                reader.close();
+                reject(e);
+            },
+            file
+        });
+    });
+}
+
+document.getElementById("inputFile").onchange = async event => {
+    const files = (() => {
+        const collator = new Intl.Collator("en", {numeric: true, sensitivity: "base"});
+        return [...(event.target.files || event.dataTransfer.files)].sort((a, b) => collator.compare(a.name, b.name));
+    })();
+
+    for(let i = 0; i < files.length; i++) {
+        const reader = await getDocument(files[i]);
+        showPDFImages(reader, reader.numPages);
+    }
     event.target.value = null;
+}
+
+window.onclick = () => {
+    const inputBox = document.getElementById("inputFile");
+    if(inputBox.hasAttribute("hide")) inputBox.removeAttribute("hide");
+    else inputBox.setAttribute("hide", true);
+}
+
+window.oncontextmenu = e => {
+    e.preventDefault();
 }
